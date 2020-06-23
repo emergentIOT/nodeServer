84332773aee9810e13e8a09cd683178927706f60
samandeep <amandeep.singh@deakin.edu.au>
2020-06-23 15:08:04 +1000

    fetch POST request to server

--- index.js
+++ index.js
@@ -3,8 +3,24 @@ const app = express();
 
 app.listen(3000, () => console.log('listening at 3000'));
 
-//Hosting static dir
+//request server to host static dir
 app.use(express.static('public'));
+
+//request server to receive json data
+app.use(express.json({limit: '1mb'}));
+
+app.post("/api", (req, res) => {
+
+    //receive the object from client side.
+    console.log(req.body);
+    const data = req.body;
+    //complete the request , send response back.
+    res.json({
+        status: "success",
+        latitude: data.lat,
+        longitude: data.long
+    });
+});
 
 //Save to database
 
