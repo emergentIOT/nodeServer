1b79e459c068cd4c46b7e5168cc79496a3b4d83d
samandeep <amandeep.singh@deakin.edu.au>
2020-06-23 14:26:44 +1000

    gelolocation data

--- index.js
+++ index.js
@@ -3,4 +3,9 @@ const app = express();
 
 app.listen(3000, () => console.log('listening at 3000'));
 
+//Hosting static dir
 app.use(express.static('public'));
+
+//Save to database
+
+//Authentication
